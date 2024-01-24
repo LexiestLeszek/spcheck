de8bb068ba128a6f5afe98dc02a4fccea07eb8fd
Leszek Mielnikow <63610219+LexiestLeszek@users.noreply.github.com>
2024-01-24 21:19:56 +0100

    bug fixes

--- spcheck.py
+++ spcheck.py
@@ -1,15 +1,13 @@
-import numpy as np
-from collections import Counter
 
 def levenshtein_distance(s1, s2):
     if len(s1) < len(s2):
-        return levenshtein_distance(s2, s1)
+        s1, s2 = s2, s1
 
     if len(s2) == 0:
         return len(s1)
 
-    previous_row = range(len(s2) + 1)
-    
+    previous_row = list(range(len(s2) + 1))
+
     for i, c1 in enumerate(s1):
         current_row = [i + 1]
         for j, c2 in enumerate(s2):
